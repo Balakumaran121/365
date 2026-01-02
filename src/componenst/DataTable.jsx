abf89d0491a36387c95b1c56fd1379c614ce4a5c
Balakumaran <balakumaran@cloudrevelinnovation.com>
2026-01-02 14:31:33 +0530

    Added functionality for sorting table data

--- src/componenst/DataTable.jsx
+++ src/componenst/DataTable.jsx
@@ -50,7 +50,7 @@ const DataTable = ({ data, columns }) => {
                         <div key={headerGroup.id} className="grid bg-gray-100 border-b" style={{ gridTemplateColumns: `repeat(${headerGroup.headers.length},1fr)` }}>
                             {
                                 headerGroup.headers.map((header) => (
-                                    <div key={header.id} className="p-3 font-semibold cursor-pointer border-r last:border-0">
+                                    <div key={header.id} className="p-3 font-semibold cursor-pointer border-r last:border-0 flex gap-2" onClick={header.column.getToggleSortingHandler()}>
                                         {flexRender(header.column.columnDef.header, header.getContext())}
                                         {{ asc: <ChevronDown />, desc: <ChevronUp /> }[header.column.getIsSorted()] ?? null}
                                     </div>
