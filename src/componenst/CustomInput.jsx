87011172ad136253fcc167a1648a4c6484c80cbc
Balakumaran <balakumaran@cloudrevelinnovation.com>
2026-01-08 14:34:40 +0530

    Updated the CustomInput component

--- src/componenst/CustomInput.jsx
+++ src/componenst/CustomInput.jsx
@@ -5,7 +5,7 @@ const CustomInput = ({ placeholder, name, label,type="text",formik,options=[] })
     return (
         <div className='flex flex-col gap-2 px-2'>
             <label htmlFor={name} className='text-lg font-semibold'>{label}</label>
-          {type==="text"?
+          {(type==="text"||type==="date")?
           <input id={name} name={name} type={type} placeholder={placeholder} value={formik.values[name]??""} onChange={formik.handleChange} onBlur={formik.handleBlur} className='border border-black p-2 rounded-lg' />:
           <select id={name} name={name}  value={formik.values[name]??""} onChange={formik.handleChange} onBlur={formik.handleBlur} className='border border-black p-2 rounded-lg'>
             <option value=""></option>
